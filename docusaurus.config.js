8695e34dca4bed2979ab60a8e1eada4842b40af9
cedoor <me@cedoor.dev>
2021-10-08 18:56:18 +0200

    chore: update code theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
-const lightCodeTheme = require("prism-react-renderer/themes/github")
-const darkCodeTheme = require("prism-react-renderer/themes/dracula")
+const lightCodeTheme = require("prism-react-renderer/themes/palenight")
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark")
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
